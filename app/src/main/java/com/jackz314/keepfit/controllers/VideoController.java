c5619ed4f7735d275666d6273995836e1645ea35
Andrew Han <dongilha@usc.edu>
2021-03-20 18:09:23 -0700

    added uploading video

--- app/src/main/java/com/jackz314/keepfit/controllers/VideoController.java
+++ app/src/main/java/com/jackz314/keepfit/controllers/VideoController.java
@@ -1,6 +1,30 @@
 package com.jackz314.keepfit.controllers;
 
 public class VideoController {
+    public String title;
 
 
+
+    public String url;
+
+    public VideoController(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
