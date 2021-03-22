98e66d392de6a47bbc330150a095912aaf3d5a42
Andrew Han <dongilha@usc.edu>
2021-03-21 20:35:56 -0700

    edited category to array in UploadVideo

--- app/src/main/java/com/jackz314/keepfit/views/UploadVideoActivity.java
+++ app/src/main/java/com/jackz314/keepfit/views/UploadVideoActivity.java
@@ -58,7 +58,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static android.graphics.Bitmap.Config.ALPHA_8;
@@ -209,8 +211,16 @@ public class UploadVideoActivity extends AppCompatActivity {
                 String link = uri.toString();
                 Timestamp timestamp = now();
 
+                String raw = categoryText.getText().toString();
+                String[] categoryArray = raw.split(",");
+                List<String> categoryList = new ArrayList<String>();
+
+                for(int i = 0; i < categoryArray.length; ++i){
+                    categoryList.add(categoryArray[i]);
+                }
+
                 Map<String, Object> media = new HashMap<>();
-                media.put("categories",categoryText.getText().toString());
+                media.put("categories", categoryList);
                 media.put("creator", uidRef);
                 media.put("is_livestream", false);
                 media.put("link", link);
