dbd228399c16cd7c2ec6b9081851c616ca947b0f
Brittany Rollins <bmrollin@usc.edu>
2021-03-22 02:03:35 -0400

    updated follow button logic

--- app/src/main/java/com/jackz314/keepfit/views/FollowActivity.java
+++ app/src/main/java/com/jackz314/keepfit/views/FollowActivity.java
@@ -3,24 +3,36 @@ package com.jackz314.keepfit.views;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.jackz314.keepfit.R;
 import com.jackz314.keepfit.controllers.UserController;
 import com.jackz314.keepfit.models.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,6 +40,8 @@ public class FollowActivity extends AppCompatActivity {
     FirebaseUser curruser = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     Boolean following;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +55,6 @@ public class FollowActivity extends AppCompatActivity {
         //User other_user = new User();
 
 
-
-
         if (other_user != null) {
             TextView other_name = findViewById(R.id.user_name_text);
             other_name.setText(other_user.getName());
@@ -54,23 +66,43 @@ public class FollowActivity extends AppCompatActivity {
         }
 
         Button follow_btn = findViewById(R.id.followButton);
-        //if following user, then button says unfollow, if not following user, button says follow
-        DocumentReference doc = db.collection("users").document(curruser.getUid()).collection("following").document(other_user.getUid());
-        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        follow_btn.setText("Unfollow");
-                        following = true;
-                    } else {
-                        follow_btn.setText("Follow");
-                        following = false;
+
+
+
+
+        db.collection("users").document(curruser.getUid()).collection("following").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot findFollowingCollection = task.getResult();
+                            if (findFollowingCollection.isEmpty()) {
+                                //if it's empty then user is not following anyone => don't want to work with null values
+                                follow_btn.setText("Follow");
+                                following = false;
+                            } else {
+                                db.collection("users").document(curruser.getUid()).collection("following")
+                                        .whereEqualTo("ref", db.collection("users").document(other_user.getUid())).get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                // BTW, `getResult()` will throw an exception if the task fails unless you first check for `task.isSuccessful()`
+                                                if (task.isSuccessful()) {
+                                                    QuerySnapshot isFollowing = task.getResult();
+                                                    if (isFollowing.isEmpty()) {
+                                                        follow_btn.setText("Follow");
+                                                        following = false;
+                                                    } else {
+                                                        follow_btn.setText("Unfollow");
+                                                        following = true;
+                                                    }
+                                                }
+                                            }
+                                        });
+                            }
+                        }
                     }
-                }
-            }
-        });
+                });
 
 
         follow_btn.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +114,8 @@ public class FollowActivity extends AppCompatActivity {
                 } else {
                     ucontrol.follow(other_user);
                 }
+                finish();
+                startActivity(getIntent());
             }
         });
     }
