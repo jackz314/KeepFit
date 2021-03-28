5c175bc82400e8397d45c34ce3fb385927a1b6cc
Jared Kuo <jaredkuo@usc.edu>
2021-03-28 16:35:16 -0700

    added login test

---
+++ app/src/androidTest/java/com/jackz314/keepfit/views/LoginTest.java
@@ -0,0 +1,114 @@
+package com.jackz314.keepfit.views;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.jackz314.keepfit.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void loginTest() throws InterruptedException {
+        ViewInteraction supportVectorDrawablesButton = onView(
+                allOf(withId(R.id.email_button), withText("Sign in with email"),
+                        childAtPosition(
+                                allOf(withId(R.id.btn_holder),
+                                        childAtPosition(
+                                                withId(R.id.container),
+                                                0)),
+                                0)));
+        supportVectorDrawablesButton.perform(scrollTo(), click());
+
+        Thread.sleep(1000);
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(2131297098)));
+        textInputEditText.perform(scrollTo(), click());
+        textInputEditText.perform(replaceText("logintest@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.button_next), withText("Next"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_top_layout),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.ScrollView")),
+                                                0)),
+                                2)));
+        materialButton.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(2131298336)));
+        textInputEditText2.perform(scrollTo(), click(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.button_done), withText("Sign in"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                4)));
+        materialButton2.perform(scrollTo(), click());
+
+        Thread.sleep(1000);
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.user_email_text),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        textView.check(matches(withText("logintest@gmail.com")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
