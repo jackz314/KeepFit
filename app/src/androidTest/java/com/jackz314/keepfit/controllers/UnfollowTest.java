f37844fbe59c067c8b86be693a5e3cec7d1899e5
Jack Zhang <zhangmengyu10@gmail.com>
2021-03-29 05:06:10 -0700

    Finish.

--- app/src/androidTest/java/com/jackz314/keepfit/controllers/UnfollowTest.java
+++ app/src/androidTest/java/com/jackz314/keepfit/controllers/UnfollowTest.java
@@ -6,6 +6,7 @@ import com.jackz314.keepfit.controllers.UserControllerKt;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UnfollowTest {
@@ -13,6 +14,11 @@ public class UnfollowTest {
     Boolean unfollowingWorked = true;
 
     FirebaseFirestore db;
+
+//    @Test
+//    public void unfollowDummy() {
+//        assertFalse(false);
+//    }
 
     @Test
     public void unfollow() throws InterruptedException {
