7d355f956b09c51fc845e2077cc9d6312b2d1147
Brittany Rollins <bmrollin@usc.edu>
2021-03-28 21:55:59 -0400

    thread.sleep => idling resource

--- app/src/androidTest/java/com/jackz314/keepfit/views/FollowUITest.java
+++ app/src/androidTest/java/com/jackz314/keepfit/views/FollowUITest.java
@@ -1,17 +1,26 @@
 package com.jackz314.keepfit.views;
 
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import static androidx.activity.result.ActivityResultCallerKt.registerForActivityResult;
 import static androidx.test.InstrumentationRegistry.getInstrumentation;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -23,11 +32,13 @@ import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
 import com.jackz314.keepfit.R;
+import com.jackz314.keepfit.TestIdlingResource;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -40,12 +51,21 @@ import static org.hamcrest.Matchers.is;
 @RunWith(AndroidJUnit4.class)
 public class FollowUITest {
 
+    private IdlingResource idlingResource;
+
+    @Before
+    public void beforeClass() throws Exception {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        idlingResource = TestIdlingResource.countingIdlingResource;
+        IdlingRegistry.getInstance().register(idlingResource);
+    }
+
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void followUITest() throws InterruptedException {
-        Thread.sleep(3000);
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.navigation_feed), withContentDescription("Feed"),
                         isDisplayed()));
@@ -56,6 +76,7 @@ public class FollowUITest {
                         isDisplayed()));
         actionMenuItemView.perform(click());
 
+        Thread.sleep(1000);
         ViewInteraction searchAutoComplete = onView(
                 allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
                         isDisplayed()));
