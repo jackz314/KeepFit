f6b2d2ac3ae7a1369a9529defef9476c7d9a589d
Jack Zhang <zhangmengyu10@gmail.com>
2021-05-09 21:49:13 -0700

    Fix update profile image.

--- app/src/main/java/com/jackz314/keepfit/views/UpdateProfileActivity.java
+++ app/src/main/java/com/jackz314/keepfit/views/UpdateProfileActivity.java
@@ -1,29 +1,13 @@
 package com.jackz314.keepfit.views;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -33,6 +17,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -44,14 +34,12 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.jackz314.keepfit.R;
-import com.jackz314.keepfit.models.User;
 
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 
@@ -109,13 +97,7 @@ public class UpdateProfileActivity extends AppCompatActivity {
         btnSelect = findViewById(R.id.btnChoose);
         imageView = findViewById(R.id.imgView);
 
-        btnSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                selectImage();
-            }
-        });
+        btnSelect.setOnClickListener(v -> selectImage());
 
 
         Button finishEdit = findViewById(R.id.finish_new_user_btn);
@@ -211,7 +193,6 @@ public class UpdateProfileActivity extends AppCompatActivity {
                         .update(user)
                         .addOnSuccessListener(aVoid -> finish())
                         .addOnFailureListener(e -> Toast.makeText(UpdateProfileActivity.this, "Error updating profile", Toast.LENGTH_SHORT).show());
-                setResult(Activity.RESULT_OK);
             } else {
                Toast.makeText(UpdateProfileActivity.this, "Please wait for image to upload", Toast.LENGTH_LONG).show();
             }
